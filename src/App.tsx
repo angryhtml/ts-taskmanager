cb05614d46683962ae3f459a79de1d4572df1118
AngryHtml <123128711+angryhtml@users.noreply.github.com>
2025-11-30 00:01:38 +0100

    add modules

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+    return (
+      <div>
+        <h1>Тест - это работает!</h1>
+        <p>Если видишь этот текст - React работает</p>
+      </div>
+    )
+  }
+  
+export default App
